00794b3fdb1b7cb2e47c85aea3df24756cf4840a
Sebastien Galvagno <dev@igalvagno.info>
2026-05-22 11:50:38 +0200

    fix version

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -2,7 +2,7 @@
 #include "test_config.h"
 #include "../src/plugin_info.h"
 #include "../src/plugin.h"
-#include "../src/version.h"
+#include "version.h"
 TEST_CASE("plugin name not empty",    "[info]")    { REQUIRE(std::string(PLUGIN_NAME).size() > 0); }
 TEST_CASE("plugin_version not empty", "[version]") { REQUIRE(std::string(plugin_version()).size() > 0); }
 TEST_CASE("flag test",                "[flag]")    { REQUIRE(TEST_SHOULD_FAIL == 0); }
